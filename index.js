bf528337acd5ba697269729f6e6943a2be0c642f
shomix <shomixpetrovic@gmail.com>
2023-05-18 14:42:45 +0200

    Remove unneeded package

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const httpm = require("@actions/http-client");
+const auth = require("@actions/http-client/lib/auth");
 
 const main = async () => {
   try {
-    // `who-to-greet` input defined in action metadata file
-    //   const nameToGreet = core.getInput("who-to-greet");
-    //   console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     console.log(`Time: ${time}`);
     core.setOutput("time", time);
 
-    const http = new httpm.HttpClient();
-    const res = await http.get(
-      "http://4gpqnjktgtegbem7duhsarii2k.ingress.america.computer/"
+    const bearerToken = new auth.BearerCredentialHandler(
+      core.getInput("spheron-token")
     );
-    console.log(res);
+
+    const http = new httpm.HttpClient("http", [bearerToken]);
+    const res = await http.get(
+      "https://api-v2.spheron.network/v1/api-keys/scope"
+    );
     console.log(res.message.statusCode);
     const body = await res.readBody();
     console.log(body);
