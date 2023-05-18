9791c5a08ea4e29d4caa5a3967d15d2eb53bdfa8
shomix <shomixpetrovic@gmail.com>
2023-05-18 14:13:48 +0200

    Send http request

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const httpm = require("@actions/http-client");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const main = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    //   const nameToGreet = core.getInput("who-to-greet");
+    //   console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    console.log(`Time: ${time}`);
+    core.setOutput("time", time);
+
+    const http = new httpm.HttpClient();
+    const res = await http.get(
+      "http://4gpqnjktgtegbem7duhsarii2k.ingress.america.computer/"
+    );
+    console.log(res);
+    console.log(res.message.statusCode);
+    const body = await res.readBody();
+    console.log(body);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
